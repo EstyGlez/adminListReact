ae35cef8390a051d991cc9800ad3fda99299efe1
EstyGlez <estibalizzmakeup@gmail.com>
2024-01-27 23:55:38 +0100

    changes

---
+++ adminList/src/components/navBar/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css'
+
+function Footer() {
+
+    return (
+        <>
+    <footer>
+        <section>© Fem-Coders</section>
+    </footer>
+        </>
+    )
+}
+
+export default Footer
--- adminList/src/main.jsx
+++ adminList/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AdminList from './views/AdminList.jsx'
+import Footer from './components/navBar/Footer/Footer.jsx'
+import Navbar from './components/navBar/NavBar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-   
   <AdminList/>
-
+  <Navbar/>
+  <Footer/>
   </React.StrictMode>,
 )
