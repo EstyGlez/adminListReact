35e59d7af2300381390b1a37da486f8217a752cd
EstyGlez <estibalizzmakeup@gmail.com>
2024-01-27 18:25:19 +0100

    changes

--- adminList/src/main.jsx
+++ adminList/src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '../src/views/admiList.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <App />
+    <adminList.jsx/>
+
   </React.StrictMode>,
 )
